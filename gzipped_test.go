fa3a5d4565549c2af17071383b912a55b3888b33
Frederik Ring <frederik.ring@gmail.com>
2018-06-22 11:04:40 +0200

    add test for library

---
+++ gzipped_test.go
@@ -0,0 +1,59 @@
+package gzipped
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHumanize(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    uint64
+		expected string
+	}{
+		{"bytes", 88, "88B"},
+		{"kilobytes", 12346, "12.1K"},
+		{"megabytes", 2434622, "2.3M"},
+		{"gigabytes", 54346229102, "50.6G"},
+		{"terabytes", 243462291021234, "221.4T"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := humanize(test.input)
+			if test.expected != result {
+				t.Errorf("Expected %v, got %v", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		name             string
+		input            []byte
+		expectedOutBytes uint64
+	}{
+		{
+			"default",
+			[]byte("this is a string that tests whatever \n happens when we gzip its contents"),
+			87,
+		},
+		{
+			"empty",
+			[]byte(""),
+			28,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(test.input)
+			result, err := Compare(buf)
+			if err != nil {
+				t.Fatalf("Unexpected error %v", err)
+			}
+			if test.expectedOutBytes != result.OutBytes {
+				t.Errorf("Expected %v out bytes, got %v", test.expectedOutBytes, result.OutBytes)
+			}
+		})
+	}
+}
